766b9ca8e71604094f4352ccb586bb313353675c
unknown <julius.risky@gmail.com>
2020-03-13 21:02:44 +0700

    belajar input data bilangan bulat

---
+++ BAB_2/input-integer.py
@@ -0,0 +1,14 @@
+nama = "arimbi"
+umur = 21
+print("nama: %s, umur: %d" % ("arimbi",21))
+print("\n",100*"=")
+
+def main():
+    #membuat prompt untuk tipe data integer
+    bil = int(input("masukkan bilangan bulat: "))
+    hasil = bil + 11
+    print("bilangan yang dimasukkan adala %d" %bil)
+    print("%d + 11 = %d" %(bil,hasil    )) 
+
+if __name__ =="__main__":
+    main()
