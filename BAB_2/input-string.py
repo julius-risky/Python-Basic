066707db98c6990283d2a6c1a8bc7ffce1cc0c7f
unknown <julius.risky@gmail.com>
2020-03-13 21:01:39 +0700

    belajar input tipe data string

---
+++ BAB_2/input-string.py
@@ -0,0 +1,12 @@
+def main():
+    #membuat prompt untuk tipe data string
+    nama = input("tuliskan nama anda: ")
+    #tipe data Integer
+    umur = input("berapa umur anda sekarang: ")
+
+    #menampilkan
+    print("hallo, perkenalkan nama saya",nama)
+    print("saya sekarang berumur",umur)
+
+if __name__ == "__main__":
+    main()
