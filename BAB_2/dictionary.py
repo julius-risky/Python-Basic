e0fffdabcadbc725cc064f51bba783fda84d9783
unknown <julius.risky@gmail.com>
2020-03-14 20:34:31 +0700

    belajar tipe data dictionary

---
+++ BAB_2/dictionary.py
@@ -0,0 +1,20 @@
+def main():
+    #membuat dan mengisi nilai ke dalam dictionary
+    d = {'nama':'julius','Nim':161414099,'alamat':'minggir'}
+    print("data asli:",d)
+    #menambahkan data
+    d['Nilai']='A' 
+    d['hobi']= 'menonton anime'
+    #menampilkan dictionary
+    print("data tambahan :",d)
+    #memodifikasi data
+    d['nama']=input("\nnama kamu: ") 
+    d['Nim']=input("Tulis Nim kamu: ")
+    d['hobi']=input("tulis hobimu: ")
+    print("\ndata sesudah di modif :",d)
+    #menghapus elemen dari dictionary
+    del d['alamat']
+    print("update data: ",d)
+
+if __name__ == "__main__":
+    main()
