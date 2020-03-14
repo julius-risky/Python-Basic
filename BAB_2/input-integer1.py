d4c4366e015ff0c201e61b6239711a7bffd6e4bf
unknown <julius.risky@gmail.com>
2020-03-14 20:42:50 +0700

    belajar input integer dengan beda

---
+++ BAB_2/input-integer1.py
@@ -0,0 +1,12 @@
+def main():
+    #membuat input data string
+    s = input("masukkan bilangan bulat: ")
+
+    Bilbulat = int(s)
+    hasil = Bilbulat + 11
+
+    print("bilangan yang di masukkan adalah %d" %Bilbulat)
+    print("%d + 11 = %d "%(Bilbulat,hasil)) 
+
+if __name__ == "__main__":
+    main()
