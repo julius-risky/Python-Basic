16b63923ba6076abc51f76b28e21a84fce6c8017
unknown <julius.risky@gmail.com>
2020-03-15 22:03:18 +0700

    latihan tipe data tuple

---
+++ BAB_2/tuple.py
@@ -0,0 +1,15 @@
+import datetime as dt
+
+def main():
+    bulan=("januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember")
+
+    today = dt.date.isoformat(dt.date.today())
+    yyyy = today[:4]
+    mm = today[5:7]
+    dd = today[8:]
+
+    print(today)
+    print("%s %s %s"% (dd,bulan[int(mm)-1],yyyy))
+
+if __name__ == "__main__":
+    main()
