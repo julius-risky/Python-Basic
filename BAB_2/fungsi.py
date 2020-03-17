917f476b44c1787f716ffa21f81019f14126be56
unknown <julius.risky@gmail.com>
2020-03-17 21:25:06 +0700

    update

--- BAB_2/fungsi.py
+++ BAB_2/fungsi.py
@@ -5,3 +5,34 @@ def modulo(a,b):
 
 h1 = modulo(10,3)
 print(h1)
+
+#=========================================================
+print(100*"=")
+
+import datetime as dt 
+
+def getisoformat(date):
+    return dt.date.isoformat(date)
+
+def day(date):
+    datestr = getisoformat(date)
+    return int(datestr[8:])
+
+def month(date):
+    datestr = getisoformat(date)
+    return int(datestr[5:7])
+
+def year(date):
+    datestr = getisoformat(date)
+    return int(datestr[:4])
+
+def main():
+    bulan=("januari","februari","maret","april","mei","juni","juli","agustus","september","oktober","november","desember")
+    today = dt.date.today()
+    print(getisoformat(today))
+    print(day(today),bulan[int(month(today)-1)],year(today))
+    print(day(today)) 
+    print(month(today))
+    print(year(today))
+if __name__ == "__main__":
+    main()
