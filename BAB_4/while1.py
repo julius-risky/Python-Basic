e68df8d06049e614f05dbd2d0a1dbe8b1ee5ab18
LENOVO <julius.risky@gmail.com>
2020-03-20 23:07:33 +0700

    latihan

---
+++ BAB_4/while1.py
@@ -0,0 +1,9 @@
+def main():
+    n = int(input("masukkan nilai n: "))
+    i = int(input("masukkan nilai awal: "))
+    while i <= n:
+        print(i)
+        i = i+1
+
+if __name__ == "__main__":
+    main()
