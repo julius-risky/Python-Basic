797f73aa103195cde4d1fe105c5b0e6107a435d8
LENOVO <julius.risky@gmail.com>
2020-03-22 20:24:43 +0700

    latihan

--- BAB_4/forelse.py
+++ BAB_4/forelse.py
@@ -2,4 +2,11 @@ def main():
     for i in range(1000, 1025):
         #pengulangan menggunakan for .. else
         for j in range(2,i):
-         
+            if i % j == 0:
+                print("%d bukan prima"%i)
+                break
+        else:
+            print("%d adalah bilangan prima"%i)
+
+if __name__ == "__main__":
+    main()
