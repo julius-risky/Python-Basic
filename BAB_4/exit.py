f613ec1890674973aaa1d91a36932660ac1828f5
LENOVO <julius.risky@gmail.com>
2020-03-22 20:23:38 +0700

    latihan

--- BAB_4/exit.py
+++ BAB_4/exit.py
@@ -3,3 +3,13 @@ def main():
     print("program pembagian bilangan")
     a = float(input("\n masukkan nilai a: "))
     b = float(input("\n masukkan nilai b: "))
+
+    if b == 0.0:
+        print("kesalahan b tidak boleh nol")
+        sys.exit(1)
+
+    c =a / b
+    print("%.2f / %.2f = %.2f"%(a,b,c))
+
+if __name__ == "__main__":
+    main()
