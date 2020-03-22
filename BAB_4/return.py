b4d2de7f67e2ce0cb21aa3969e3ee5dc8024196b
LENOVO <julius.risky@gmail.com>
2020-03-22 20:26:59 +0700

    latihan

--- BAB_4/return.py
+++ BAB_4/return.py
@@ -2,4 +2,12 @@ def tambah(a,b):
     c = a+b
     return c
 
-d
+def main():
+    x = int(input("masukkan nilai ke-1: "))
+    y = int(input("masukkan nilai ke-2: "))
+
+    hasil = tambah(x,y)
+    print("%d + %d = %d"%(x,y,hasil))
+
+if __name__ == "__main__":
+    main()
