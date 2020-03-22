86bfdb91af6517d1635e640fb36523804ad4dcfd
LENOVO <julius.risky@gmail.com>
2020-03-22 20:26:05 +0700

    latihan

--- BAB_4/fortanpaelse.py
+++ BAB_4/fortanpaelse.py
@@ -1,4 +1,15 @@
 def main():
     for i in range(10,25):
         prima = True
-      
+        for j in range (2,i):
+            if i % j ==0:
+                prima = False
+                print(j)
+                break
+        if not prima:
+            print("%d bukan prima"%i)
+        else:
+            print("%d bilangan prima"%i)
+
+if __name__ == "__main__":
+    main()
