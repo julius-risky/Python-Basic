b7f75ef7d332d39bd727f55def7f0f6408aa9e64
LENOVO <julius.risky@gmail.com>
2020-03-23 23:18:25 +0700

    latihan

--- BAB_5/passbyreference.py
+++ BAB_5/passbyreference.py
@@ -4,5 +4,16 @@ def ubahnilai(p):
     print("p\t= %d"%p)
     return
 
+def main():
+    a = 5
+    print("sebelum fungsi ubahnilai")
+    print("a \t= %d"%a )
 
+    #mengambil fungsi ubahnilai
+    #dengan a sebagai parameter aktualnya
+    ubahnilai(a)
+    print("\n Setelah pemanggilan fungsi")
+    print("a\t= %d"%a)
+
+if __name__ == "__main__":
     main()
