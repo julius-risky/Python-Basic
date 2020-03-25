b9483eb02e506d7a290117ecd16e057507f7e289
LENOVO <julius.risky@gmail.com>
2020-03-25 23:33:45 +0700

    latihan default parameter

--- BAB_5/defaultparameter.py
+++ BAB_5/defaultparameter.py
@@ -2,3 +2,12 @@ def infokaryawan(nama,usia=35):
     print("nama: %s"%nama)
     print("usia: %d"%usia, end='\n\n')
     return
+
+def main():
+    #memanggil nama karyawan
+    infokaryawan("bimo") #mengisi usia sesuai parameter default
+    infokaryawan("haryo",33)
+    infokaryawan("juki",22)
+
+if __name__ == "__main__":
+    main()
