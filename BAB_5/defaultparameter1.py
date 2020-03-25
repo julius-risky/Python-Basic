55b2b8aa8c6534fd3804c5f22ddca7afe65c5f63
LENOVO <julius.risky@gmail.com>
2020-03-25 23:34:20 +0700

    latihan default parameter

--- BAB_5/defaultparameter1.py
+++ BAB_5/defaultparameter1.py
@@ -5,5 +5,14 @@ def cetak(alist=[], urut=False):
         print(i, end=' ')
     print()
     return
-__main__":
+
+def main():
+    li=[55,62,72,88,90,12]
+    #memanggil fungsi cetak
+    print("data tidak urut")
+    cetak(li)
+    print("data urut")
+    cetak(li,True)
+
+if __name__ == "__main__":
     main()
