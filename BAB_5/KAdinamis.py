6080e6c5564c5309048080cc61d206dce8eef8e3
LENOVO <julius.risky@gmail.com>
2020-03-26 23:32:04 +0700

    latihan

--- BAB_5/KAdinamis.py
+++ BAB_5/KAdinamis.py
@@ -2,5 +2,9 @@ def cetakdict(**kwarg):
     print(type(kwarg))
     print(kwarg)
     return
+def main():
+    cetakdict(satu=10,dua=30,tiga=40)
+    cetakdict(buku='ipa',nama='juki',tgl='3 juni')
 
-cetakdict(satu=10,dua=30,tiga=40)
+if __name__ == "__main__":
+    main()
