f210493c35ba63d5905c29c65e7463c607312f14
LENOVO <julius.risky@gmail.com>
2020-03-26 23:26:16 +0700

    latihan

--- BAB_5/keywordargument.py
+++ BAB_5/keywordargument.py
@@ -4,3 +4,9 @@ def info(nama,usia,gaji):
     print("Gaji : %d"%gaji,end='\n\n')
     return
 
+def main():
+    info('bimo',25,4000000)
+    info('juki',25,8000000)
+
+if __name__ == "__main__":
+    main()
