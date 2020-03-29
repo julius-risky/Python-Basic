c72a15561c4c325d20f0712db0e4b14ba685738d
LENOVO <julius.risky@gmail.com>
2020-03-29 23:31:10 +0700

    latihan fungsi lamda:

---
+++ BAB_5/lamdafunc.py
@@ -0,0 +1,27 @@
+#mendefinisikan fungsi pemanggil
+#parameter dan nilai kebalian berupa fungsi
+def panggil(func):
+    return func
+
+def helloword():
+    return "Hello world"
+
+def main():
+    #mendefinisikan list
+    daftarnama =["Dewi","bimo","Aji","BAYU","sakit"]
+    print("keadaan awal: ")
+    print(daftarnama)
+
+    #mengurutkan elemen list dengan dhorted()
+    print("\nMenggunakan sorted(): ")
+    print(sorted(daftarnama))
+
+    #melewatkan lambda func untuk mengurutkan
+    #elemen tanpa memperdulikan huruf kecil/besar
+    daftarnama.sort(key=lambda n: n.lower())
+
+    print("\nkeadaan akhir: ")
+    print(daftarnama)
+
+if __name__ == "__main__":
+    main()
