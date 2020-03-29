c093ecf5724ffb1090e43ea8943cf90bd29cd328
LENOVO <julius.risky@gmail.com>
2020-03-29 23:34:14 +0700

    latihan fungsi overload var-lenght argumen

---
+++ BAB_5/overload2.py
@@ -0,0 +1,10 @@
+def tambah(*bil):
+    hasil = 0
+    for i in bil:
+        hasil += i
+    return hasil
+
+def main():
+    print("jumlah(3,4,5,6)\t\t: ",tambah(3,4,5,6))
+    print("jumlah(5,6,7,8,9,10)\t: ",tambah(5,6,7,8,9,10))
+
