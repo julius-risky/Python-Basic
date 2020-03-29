41dfa8b6eeb08be6f0cf7864a965c65ba8379b46
LENOVO <julius.risky@gmail.com>
2020-03-29 23:35:02 +0700

    latihan fungsi rekursi

---
+++ BAB_5/rekursi.py
@@ -0,0 +1,17 @@
+import sys
+
+def faktorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * faktorial(n-1)
+
+def main():
+    bil = int(input("Masukkan nilai faktorial yang anda inginkan: "))
+
+    if bil < 0:
+        print("EROR: Bilangan tidak boleh negatif")
+        sys.exit(1)
+    
+
+    main()
