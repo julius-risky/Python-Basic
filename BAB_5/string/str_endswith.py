a25b781e49c9a9ea753f24d14f5988e4fab9ba4d
LENOVO <julius.risky@gmail.com>
2020-03-31 23:51:01 +0700

    latihan

--- BAB_5/string/str_endswith.py
+++ BAB_5/string/str_endswith.py
@@ -1,3 +1,8 @@
 def main():
+    str = "hello world!!"
+    print(str.endswith("world"))
+    print(str.endswith("world!!"))
+    print(str.endswith("lo",3,5))
 
+if __name__ == "__main__":
     main()
