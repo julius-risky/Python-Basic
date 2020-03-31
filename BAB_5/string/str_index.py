9caf935fdc15269e459933ab01550a9516ec1788
LENOVO <julius.risky@gmail.com>
2020-03-31 23:52:29 +0700

    latihan

--- BAB_5/string/str_index.py
+++ BAB_5/string/str_index.py
@@ -1,4 +1,10 @@
 def main():
     str = "hello world!!"
 
-   
+    print("str: ",str)
+    print("str.index('o'): ",str.index('o'))
+    print("str.indeks('o',5)",str.indeks('o',5))
+    print("str.indeks('world')",str.indeks('world'))
+
+if __name__ == "__main__":
+    main()
