ffad8f5f96eb2d6231ad7ed19d44f4b2d00c91f9
LENOVO <julius.risky@gmail.com>
2020-03-31 23:50:23 +0700

    latihan

--- BAB_5/string/str_count.py
+++ BAB_5/string/str_count.py
@@ -1,2 +1,12 @@
 def main():
-    
+    str = "hello world!!"
+    a = str.count('l')
+    b = str.count('o')
+    c = str.count('l',1,6)
+
+    print("Huruf 'l' dalam str: ",a)
+    print("huruf 'o' dalam str: ",b)
+    print("huruf 'l' dari indeks 1-6: ", c)
+
+if __name__ == "__main__":
+    main()
