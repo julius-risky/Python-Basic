b82821364bf359f481ac60efe385be8d9d66621f
LENOVO <julius.risky@gmail.com>
2020-04-02 23:41:47 +0700

    latihan OOP

---
+++ BAB_6/metode.py
@@ -0,0 +1,15 @@
+#mendefinisikan kelas
+class kotak(object):
+    def __init__(self,p,l,t):
+        self.panjang = p
+        self.lebar = l
+        self.tinggi = t
+    def hitungvolume(self):
+        return self.panjang * self.lebar *self.tinggi
+    def cetakData(self):
+        print("\nobject" + self)
+        print("Panjang\t: ",self.panjang)
+        print("lebar\t: ",self.lebar)
+        print("tinggi\t: ",self.tinggi)
+    def cetakVolume(self):
+        print("Volume\t: ",self.hitungvolume())
