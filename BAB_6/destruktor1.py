32c777bf46db826a3f00838f79d12414a81745a4
LENOVO <julius.risky@gmail.com>
2020-04-04 20:31:35 +0700

    latihan destruktor

---
+++ BAB_6/destruktor1.py
@@ -0,0 +1,10 @@
+#mendefinisikan kelas
+class myfile(object):
+    def __init__(self, namafile):
+        self.file = open(namafile)
+    def __del__(self):
+        self.file.close()
+    def bacadata(self):
+        for baris in self.file:
+            print(baris,end='')
+
