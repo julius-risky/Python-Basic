5d2dd81e8e865af6d1cd5e134cda3f57de0d1d99
LENOVO <julius.risky@gmail.com>
2020-04-06 22:31:27 +0700

    laatihan metode statis @statistismetode

---
+++ BAB_6/metodestatis1.py
@@ -0,0 +1,30 @@
+import math
+
+class hitung(object):
+    def __init__(self,p,l,t):
+        pass
+
+    @staticmethod
+    def log10(x):
+        return math.log10(x)
+
+    @staticmethod
+    def log(x):
+        return math.log(x)
+    
+    @staticmethod
+    def kali(x,y):
+        return x * y
+
+    @staticmethod
+    def pangkat(x,y):
+        return math.pow(x,y)
+    
+    @staticmethod
+    def akar(x):
+        return math.sqrt(x)
+    
+    @staticmethod
+    def mutlak(x):
+        return abs(x)
+
