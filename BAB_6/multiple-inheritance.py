c33f437657c791ac873ce3160feb2b7abca49efb
LENOVO <julius.risky@gmail.com>
2020-04-08 15:23:35 +0700

    latihan

--- BAB_6/multiple-inheritance.py
+++ BAB_6/multiple-inheritance.py
@@ -3,3 +3,10 @@ class induk1(object):
         self.a = a
     def cetakA(self):
         print("nilai a = ",self.a)
+
+class induk2(object):
+    def __init__(self,b):
+        self.b = b
+    def cetakB(self):
+        print("nilai b = ",self.b)
+
