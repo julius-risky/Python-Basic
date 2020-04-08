8e797c239c7880e4eaa651dbbe2ba65a3daa72ca
LENOVO <julius.risky@gmail.com>
2020-04-08 15:21:26 +0700

    latihan

--- BAB_6/pewarisan.py
+++ BAB_6/pewarisan.py
@@ -13,3 +13,25 @@ class kotak(object):
     def cetakVolume(self):
         print("Volume\t: ",self.hitungvolume())
 #mendefinisikan turunan (subclass)
+class kotakwarna(kotak):
+    def __init__(self,p,l,t,w):
+        self.panjang = p
+        self.lebar = l
+        self.tinggi = t
+        self.warna = w
+    def cetakData(self):
+        print("Panjang\t: ",self.panjang)
+        print("lebar\t: ",self.lebar)
+        print("tinggi\t: ",self.tinggi)
+        print("warna\t: ",self.warna)
+
+def main():
+    #membuat object kotak warna
+    kotakwarna1 = kotakwarna(6,5,4,"kuning")
+
+    #menggunakan object
+    kotakwarna1.cetakData()
+    kotakwarna1.hitungvolume()
+
+if __name__ == "__main__":
+    main()
