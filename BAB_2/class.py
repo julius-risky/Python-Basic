0a757eaa4f658f00a200ee164e7698d1382049d6
LENOVO <julius.risky@gmail.com>
2020-10-13 15:06:03 +0700

    latihan class

--- BAB_2/class.py
+++ BAB_2/class.py
@@ -1,33 +1,38 @@
-#mendefinisikan kelas node
+# mendefinisikan kelas node
 class node:
     def __init__(self, element, next=None):
         self.element = element
         self.next = next
 
-#mendefinisikan kelass linkedlist
+
+# mendefinisikan kelass linkedlist
 class linkedlist:
     def __init__(self):
         self.front = None
         self.back = None
         self.size = 0
+
     def isEmpety(self):
         return self.size == 0
+
     def addfrist(self, element):
         if self.isEmpety():
             self.front = node(element)
             self.back = self.front
         else:
-           temp = self.front
-           self.front = node(element, temp)
-        self.size +=1
-    def addlast(self,element):
+            temp = self.front
+            self.front = node(element, temp)
+        self.size += 1
+
+    def addlast(self, element):
         if self.isEmpety():
             self.front = node(element)
             self.back = self.front
         else:
-            self.back.next= node(element)
+            self.back.next = node(element)
             self.back = self.back.next
         self.size += 1
+
     def printlist(self):
         print("isi linked list: ")
         current = self.front
@@ -35,19 +40,20 @@ class linkedlist:
             print(current.element)
             current = current.next
 
+
 def main():
-    #membuat objek dari kelas linkedlist
+    # membuat objek dari kelas linkedlist
     li = linkedlist()
 
     li.addfrist(input("masukkan nilai awal: "))
-    #menambahkan elemen di awal
-    
-    #menambahkan elemen di akhir
+    # menambahkan elemen di awal
+
+    # menambahkan elemen di akhir
     li.addlast(20)
     li.addlast(30)
 
     li.printlist()
+
+
 if __name__ == "__main__":
     main()
-
-    
